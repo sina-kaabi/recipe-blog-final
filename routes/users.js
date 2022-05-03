38675027ef726fff2b3df9ae918a6f62b8645a79
“Sina <johndoe@example.com>
2022-05-03 01:21:35 +0100

    building a rest api with express

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+
+
+//Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+
+})
+
+//Getting One
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+
+})
+
+// Creating One
+
+router.post('/', (req, res) => {
+
+})
+
+//Updating One
+
+router.patch('/:id', (req, res) => {
+    
+})
+
+//Deleting One
+
+router.delete('/:id', (req, res) => {
+
+})
+
+
+module.exports = router
